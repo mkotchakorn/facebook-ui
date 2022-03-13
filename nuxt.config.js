ab9b52774c88314471f7af381234b629dc7b4fc8
Kotchakorn M <mill1704.bn@gmail.com>
2022-03-13 18:15:10 +0700

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ export default {
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
+  ssr: true,
+  target: 'static',
+
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['~/assets/css/main.css'],
 
