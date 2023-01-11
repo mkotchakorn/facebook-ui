438f81f5d2a0334c13db66b905532112aaf593a2
Kotchakorn M <32461094+mill1704@users.noreply.github.com>
2023-01-12 00:02:57 +0700

    fix

--- components/Timeline/VideoRoom.vue
+++ components/Timeline/VideoRoom.vue
@@ -3,7 +3,7 @@
     <div class="card flex items-center flex-nowrap">
         <div class="rounded-full border border-blue-100 h-10 flex items-center justify-center max-w-[148px] hover:bg-gray-200 px-3 cursor-pointer">
             <img src='https://fakeimg.pl/24x24' class="rounded-full mr-2" />
-            <p class="text-[15px] text-primary font-semibold">Create Room</p>
+            <p class="text-[14px] text-primary font-semibold">Create Room</p>
         </div>
         <div class="profile flex items-center flex-1">
             <img v-for="i in 10" :key="i" src='https://fakeimg.pl/40x40' class="rounded-full ml-4" />
